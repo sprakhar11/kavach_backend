280329344999aa293c49ffd8bf1e372b77189f42
sprakhar11 <prakhar.infinity@gmail.com>
2023-04-23 04:23:35 +0530

    i4

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path=require("path");
 const app=express();
 const port=process.env.PORT||8000;
 require("dotenv").config();
+dotenv.config();
 const mongoose=require("mongoose");
 const connectDb=require("./database/database.js");
 const DATABASE_URL=process.env.DATABASE_URL;
