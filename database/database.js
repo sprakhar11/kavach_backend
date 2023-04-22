ec4677cca2a367a2d59a550c4f7244f3ec963cee
sprakhar11 <prakhar.infinity@gmail.com>
2023-04-23 04:12:25 +0530

    i

--- database/database.js
+++ database/database.js
@@ -1,9 +1,9 @@
 const mongoose=require("mongoose");
 
-require("dotenv").config({ path: 'ENV_FILENAME' });
+require("dotenv").config();
 
 const connectDb=(DATABASE_URL)=>{
-    return mongoose.connect(DATABASE_URL).then((success)=>{
+    return mongoose.connect(DATABASE_URL, { useNewUrlParser: true }).then((success)=>{
         console.log("Database connected Successfully");
     }).catch((error)=>{
         console.log("Database not connected");
